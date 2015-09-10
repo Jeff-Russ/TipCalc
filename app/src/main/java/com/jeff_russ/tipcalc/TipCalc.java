7fdcbacd258887346b4bc61f641b0e30ae5de044
Jeff-Russ <jeffreylynnruss@gmail.com>
2015-09-10 18:11:04 -0400

    New theme, refactored vars, new form field

--- app/src/main/java/com/jeff_russ/tipcalc/TipCalc.java
+++ app/src/main/java/com/jeff_russ/tipcalc/TipCalc.java
@@ -10,44 +10,51 @@ import android.widget.EditText;
 
 public class TipCalc extends AppCompatActivity {
 
-    // SAVE STATE!
-    private static final String TOTAL_BILL = "TOTAL_BILL";
-    private static final String CURRENT_TIP = "CURRENT_TIP";
-    private static final String BILL_WITHOUT_TIP = "BILL_WITHOUT_TIP";
+    // Saved states as strings.
+    private static final String BILL_BEFORE_TIP = "BILL_BEFORE_TIP";
+    private static final String TIP_PERCENT = "TIP_PERCENT";
+    private static final String FINAL_BILL = "FINAL_BILL";
+    private static final String TIP_AMOUNT = "TIP_AMOUNT";
 
-    // variable for active data:
+    // active states as doubles
     private double billBeforeTip;
-    private double tipAmount;
+    private double tipPercent;
     private double finalBill;
+    private double tipAmount;
 
-    // for access to UI objects:
+    // declare java versions of UI elements:
     EditText billBeforeTipET;
-    EditText tipAmountET;
+    EditText tipPercentET;
     EditText finalBillET;
+    EditText tipAmountET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tip_calc);
+        super.onCreate(savedInstanceState);         // call to super
+        setContentView(R.layout.activity_tip_calc); // associate with xml layout
 
         // restore state stuff:
-        if(savedInstanceState == null){ // if this is our first startup
-            billBeforeTip = 0.0;
-            tipAmount = 0.15;
-            finalBill = 0.0;
-        }else{                          // we are coming from a restored state
-            billBeforeTip = savedInstanceState.getDouble(BILL_WITHOUT_TIP);
-            tipAmount     = savedInstanceState.getDouble(CURRENT_TIP);
-            finalBill     = savedInstanceState.getDouble(TOTAL_BILL);
+        if(savedInstanceState == null){    // if this is our first startup:
+            billBeforeTip = 0.0;    // set
+            tipPercent = 15;        // to
+            finalBill = 0.0;        // default
+            tipAmount = 0.0;        // values
+        }else{                                  // we are coming from a restored state:
+            billBeforeTip = savedInstanceState.getDouble(BILL_BEFORE_TIP);  // restore
+            tipPercent    = savedInstanceState.getDouble(TIP_PERCENT);      // from our
+            finalBill     = savedInstanceState.getDouble(FINAL_BILL);       // string
+            tipAmount     = savedInstanceState.getDouble(TIP_AMOUNT);       // constants
         }
 
-        // initialize for access to UI objects:
+        // Point our java object to the UI elements:
         billBeforeTipET = (EditText) findViewById(R.id.billEditView);
-        tipAmountET  = (EditText) findViewById(R.id.tipEditView);
+        tipPercentET    = (EditText) findViewById(R.id.percentEditView);
         finalBillET     = (EditText) findViewById(R.id.finalEditView);
+        tipAmountET     = (EditText) findViewById(R.id.tipEditView);
 
-        // add change listeners
+        // add change listeners for future user input:
         billBeforeTipET.addTextChangedListener(billBeforeTipListener);
+        // I'm not sure if we need this for tipAmountET
     }
 
     private TextWatcher billBeforeTipListener = new TextWatcher() {
@@ -57,7 +64,7 @@ public class TipCalc extends AppCompatActivity {
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-            // check for exception:
+            // check for exception (not sure why we need this, maybe if parse fails?):
             try{ billBeforeTip = Double.parseDouble(s.toString()); } // s is bill amount from UI input
             catch( NumberFormatException e ){ billBeforeTip = 0.0; }
 
@@ -65,20 +72,26 @@ public class TipCalc extends AppCompatActivity {
         }
     };
 
-    private void updateTipAndFinalBill(){   // this method is our main processing
-        double tipAmount = Double.parseDouble(tipAmountET.getText().toString());
-        double finalBill = billBeforeTip + (billBeforeTip * tipAmount); // calc final bill
-        finalBillET.setText(String.format("%.02f", finalBill));         // update UI
+    // this method is our main processing of the final amount
+    private void updateTipAndFinalBill(){
+        double tipPercent = Double.parseDouble(tipAmountET.getText().toString());
+        double tipAmount = billBeforeTip * tipPercent * 100;    // get tip amount
+        double finalBill = billBeforeTip + tipAmount;          // calc final bill
+
+        tipAmountET.setText(String.format("%.02f", tipAmount)); // update tipAmount UI
+        finalBillET.setText(String.format("%.02f", finalBill)); // update billAmount UI
     }
 
-    // called when the device changes orientation, switched out to, etc.
+    // called when the device changes orientation, switched out to other app, etc.
+    @Override
     protected void onSaveInstanceState(Bundle outState){
         super.onSaveInstanceState(outState);
 
         // save our variables to constants
-        outState.putDouble(TOTAL_BILL, finalBill);
-        outState.putDouble(CURRENT_TIP, tipAmount);
-        outState.putDouble(BILL_WITHOUT_TIP, billBeforeTip);
+        outState.putDouble(BILL_BEFORE_TIP, billBeforeTip);
+        outState.putDouble(TIP_PERCENT, tipPercent);
+        outState.putDouble(FINAL_BILL, finalBill);
+        outState.putDouble(TIP_AMOUNT, tipAmount);
     }
 
     @Override
