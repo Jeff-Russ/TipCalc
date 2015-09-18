29efc904f519835dea5715641f13d3d3aa14448b
Jeff-Russ <jeffreylynnruss@gmail.com>
2015-09-18 18:29:06 -0400

    Pretty much done
    Done but needs testing

--- app/src/main/java/com/jeff_russ/tipcalc/TipCalc.java
+++ app/src/main/java/com/jeff_russ/tipcalc/TipCalc.java
@@ -1,105 +1,356 @@
 package com.jeff_russ.tipcalc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.app.Activity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Chronometer;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class TipCalc extends AppCompatActivity {
+public class TipCalc extends Activity {
 
-    // SAVE STATE!
+    // Constants used when saving and restoring
     private static final String TOTAL_BILL = "TOTAL_BILL";
     private static final String CURRENT_TIP = "CURRENT_TIP";
     private static final String BILL_WITHOUT_TIP = "BILL_WITHOUT_TIP";
 
-    // variable for active data:
-    private double billBeforeTip;
-    private double tipAmount;
-    private double finalBill;
+    private double billBeforeTip;   // Users bill before tip
+    private double tipAmount;       // Tip amount
+    private double finalBill;       // Bill plus Tip
 
-    // for access to UI objects:
     EditText billBeforeTipET;
     EditText tipAmountET;
     EditText finalBillET;
 
+    // NEW PART ---------------
+
+    private int[] checklistValues = new int[12]; // Sum of all radio buttons and check boxes
+
+    CheckBox friendlyCheckBox;          // Declare CheckBoxes
+    CheckBox specialsCheckBox;
+    CheckBox opinionCheckBox;
+
+    RadioGroup availableRadioGroup;     // Declare RadioButtons
+    RadioButton availableBadRadio;
+    RadioButton availableOKRadio;
+    RadioButton availableGoodRadio;
+
+    Spinner problemsSpinner;            // Declare Spinner (Drop Down Box)
+
+    Button startChronometerButton;      // Declare Buttons
+    Button pauseChronometerButton;
+    Button resetChronometerButton;
+
+    Chronometer timeWaitingChronometer; // Declare Chronometer
+
+    long secondsYouWaited = 0;          // The number of seconds you spent waiting for the waitress
+
+    TextView timeWaitingTextView;       // TextView for the chronometer
+
+    // END OF NEW PART ---------------
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tip_calc);
+        setContentView(R.layout.activity_tip_calc); // Inflate the GUI
 
-        // restore state stuff:
-        if(savedInstanceState == null){ // if this is our first startup
+        // Check if app just started, or if it is being restored
+        if (savedInstanceState == null) {
+            // Just started
             billBeforeTip = 0.0;
-            tipAmount = 0.15;
+            tipAmount = .15;
             finalBill = 0.0;
-        }else{                          // we are coming from a restored state
+
+        } else {
+            // App is being restored
             billBeforeTip = savedInstanceState.getDouble(BILL_WITHOUT_TIP);
-            tipAmount     = savedInstanceState.getDouble(CURRENT_TIP);
-            finalBill     = savedInstanceState.getDouble(TOTAL_BILL);
+            tipAmount = savedInstanceState.getDouble(CURRENT_TIP);
+            finalBill = savedInstanceState.getDouble(TOTAL_BILL);
         }
+        // Initialize the EditTexts
+        billBeforeTipET = (EditText) findViewById(R.id.billEditText);   // Users bill before tip
+        tipAmountET = (EditText) findViewById(R.id.tipEditText);        // Tip amount
+        finalBillET = (EditText) findViewById(R.id.finalBillEditText);  // Bill plus tip
 
-        // initialize for access to UI objects:
-        billBeforeTipET = (EditText) findViewById(R.id.billEditView);
-        tipAmountET  = (EditText) findViewById(R.id.tipEditView);
-        finalBillET     = (EditText) findViewById(R.id.finalEditView);
+        // Initialize the SeekBar and add a ChangeListener
+        tipSeekBar = (SeekBar) findViewById(R.id.changeTipSeekBar);
 
-        // add change listeners
+        tipSeekBar.setOnSeekBarChangeListener(tipSeekBarListener);
+
+        // ---------------------------
+        // Add change listener for when the bill before tip is changed
         billBeforeTipET.addTextChangedListener(billBeforeTipListener);
-    }
 
-    private TextWatcher billBeforeTipListener = new TextWatcher() {
-        @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-        @Override public void afterTextChanged(Editable s) {}
+        // NEW PART ---------------
+
+        // Initialize CheckBoxs
+        friendlyCheckBox = (CheckBox) findViewById(R.id.friendlyCheckBox);
+        specialsCheckBox = (CheckBox) findViewById(R.id.specialsCheckBox);
+        opinionCheckBox = (CheckBox) findViewById(R.id.opinionCheckBox);
+
+        setUpIntroCheckBoxes(); // Add change listeners to check boxes
+
+        // Initialize RadioButtons
+        availableBadRadio = (RadioButton) findViewById(R.id.availableBadRadio);
+        availableOKRadio = (RadioButton) findViewById(R.id.availableOKRadio);
+        availableGoodRadio = (RadioButton) findViewById(R.id.availableGoodRadio);
+
+        // Initialize RadioGroups
+        availableRadioGroup = (RadioGroup) findViewById(R.id.availableRadioGroup);
+
+        addChangeListenerToRadios();    // Add ChangeListener To Radio buttons
+
+        problemsSpinner = (Spinner) findViewById(R.id.problemsSpinner); // Initialize the Spinner
+
+        problemsSpinner.setPrompt("Problem Solving");
+
+        addItemSelectedListenerToSpinner(); // Add ItemSelectedListener To Spinner
+
+        // Initialize Buttons
+        startChronometerButton = (Button) findViewById(R.id.startChronometerButton);
+        pauseChronometerButton = (Button) findViewById(R.id.pauseChronometerButton);
+        resetChronometerButton = (Button) findViewById(R.id.resetChronometerButton);
+
+        setButtonOnClickListeners();    // Add setOnClickListeners for buttons
+
+        // Initialize Chronometer
+        timeWaitingChronometer = (Chronometer) findViewById(R.id.timeWaitingChronometer);
+
+        // TextView for Chronometer
+        timeWaitingTextView = (TextView) findViewById(R.id.timeWaitingTextView);
+    }
+       // END OF NEW PART ---------------
+
+    // Called when the bill before tip amount is changed
+    private TextWatcher billBeforeTipListener = new TextWatcher(){
+        @Override
+        public void afterTextChanged(Editable arg0) { /* TODO Auto-generated method stub*/ }
 
         @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+            /* TODO Auto-generated method stub*/ }
 
-            // check for exception:
-            try{ billBeforeTip = Double.parseDouble(s.toString()); } // s is bill amount from UI input
-            catch( NumberFormatException e ){ billBeforeTip = 0.0; }
-
-            updateTipAndFinalBill(); // we will create this method below
+        @Override
+        public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+            try{ // Change the billBeforeTip to the new input
+                billBeforeTip = Double.parseDouble(arg0.toString());
+            } catch(NumberFormatException e){
+                billBeforeTip = 0.0;
+            }
+            updateTipAndFinalBill();
         }
     };
 
-    private void updateTipAndFinalBill(){   // this method is our main processing
+    // Update the tip amount and add tip to bill to find the final bill amount
+    private void updateTipAndFinalBill(){
+        // Get tip amount
         double tipAmount = Double.parseDouble(tipAmountET.getText().toString());
-        double finalBill = billBeforeTip + (billBeforeTip * tipAmount); // calc final bill
-        finalBillET.setText(String.format("%.02f", finalBill));         // update UI
+
+        // The bill before tip amount was set in billBeforeTipListener Get the bill plus the tip
+        double finalBill = billBeforeTip + (billBeforeTip * tipAmount);
+
+        // Set the total bill amount including the tip Convert into a 2 decimal place String
+        finalBillET.setText(String.format("%.02f", finalBill));
     }
 
-    // called when the device changes orientation, switched out to, etc.
+    // Called when a device changes in some way. For example, when a keyboard is popped out, or when
+    // the device is rotated. Used to save state information that you'd like to be made available.
+    @Override
     protected void onSaveInstanceState(Bundle outState){
         super.onSaveInstanceState(outState);
-
-        // save our variables to constants
         outState.putDouble(TOTAL_BILL, finalBill);
         outState.putDouble(CURRENT_TIP, tipAmount);
         outState.putDouble(BILL_WITHOUT_TIP, billBeforeTip);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tip_calc, menu);
-        return true;
-    }
+    private SeekBar tipSeekBar; // SeekBar used to make a custom tip
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private OnSeekBarChangeListener tipSeekBarListener = new OnSeekBarChangeListener(){
+        @Override
+        public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            // Get the value set on the SeekBar
+            tipAmount = (tipSeekBar.getProgress()) * .01;
+            // Set tipAmountET with the value from the SeekBar
+            tipAmountET.setText(String.format("%.02f", tipAmount));
+            // Update all the other EditTexts
+            updateTipAndFinalBill();
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void onStartTrackingTouch(SeekBar arg0) { /* TODO Auto-generated method stub*/ }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar arg0) { /* TODO Auto-generated method stub*/ }
+    };
+
+    // ---- NEW STUFF ----------
+
+    private void setUpIntroCheckBoxes(){
+
+        // Add ChangeListener to the friendlyCheckBox
+        friendlyCheckBox.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+
+                // Use java ternary operator to set the right values for
+                // each item on the waitress check box checklist
+                checklistValues[0] = (friendlyCheckBox.isChecked())?4:0;
+
+                setTipFromWaitressChecklist();  // Calculate tip using the waitress checklist options
+                updateTipAndFinalBill();        // Update all the other EditTexts
+            }
+        });
+
+        // Add ChangeListener to the specialsCheckBox
+        specialsCheckBox.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+                // Use java ternary operator to set the right values for
+                // each item on the waitress check box checklist
+                checklistValues[1] = (specialsCheckBox.isChecked())?1:0;
+
+                setTipFromWaitressChecklist();  // Calculate tip using the waitress checklist options
+                updateTipAndFinalBill();        // Update all the other EditTexts
+            }
+        });
+
+        // Add ChangeListener to the opinionCheckBox
+        opinionCheckBox.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+
+                // Use java ternary operator to set the right values for
+                // each item on the waitress check box checklist
+                checklistValues[2] = (opinionCheckBox.isChecked())?2:0;
+
+                setTipFromWaitressChecklist();  // Calculate tip using the waitress checklist options
+                updateTipAndFinalBill();        // Update all the other EditTexts
+            }
+        });
+    }
+
+    // Calculate tip using the waitress checklist options
+    private void setTipFromWaitressChecklist(){
+        int checklistTotal = 0;
+        // Cycle through all the checklist values to calculate a total amount based on waitress performance
+        for(int item : checklistValues){ checklistTotal += item; }
+        // Set tipAmountET
+        tipAmountET.setText(String.format("%.02f", checklistTotal * .01));
+    }
+
+    private void addChangeListenerToRadios(){
+        // Setting the listeners on the RadioGroups and handling them in the same location
+        availableRadioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener()
+        {   // checkedId is the RadioButton selected
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Use java ternary operator to set the right values for
+                // each item on the waitress radiobutton checklist
+                checklistValues[3] = (availableBadRadio.isChecked())?-1:0;
+                checklistValues[4] = (availableOKRadio.isChecked())?2:0;
+                checklistValues[5] = (availableGoodRadio.isChecked())?4:0;
+                // Calculate tip using the waitress checklist options
+
+                setTipFromWaitressChecklist();  // Calculate tip using the waitress checklist options
+                updateTipAndFinalBill();        // Update all the other EditTexts
+            }
+        });
+    }
+
+    // Adds Spinner ItemSelectedListener
+    private void addItemSelectedListenerToSpinner(){
+        problemsSpinner.setOnItemSelectedListener(new OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+                checklistValues[6] = (String.valueOf(problemsSpinner.getSelectedItem()).equals("Bad"))?-1:0;
+                checklistValues[7] = (String.valueOf(problemsSpinner.getSelectedItem()).equals("OK"))?3:0;
+                checklistValues[8] = (String.valueOf(problemsSpinner.getSelectedItem()).equals("Good"))?6:0;
+
+                setTipFromWaitressChecklist();  // Calculate tip using the waitress checklist options
+                updateTipAndFinalBill();        // Update all the other EditTexts
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) { /* TODO Auto-generated method stub*/ }
+        });
+    }
+
+    // Adds ClickListeners for buttons so they can control  the chronometer
+    private void setButtonOnClickListeners(){
+        startChronometerButton.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                int stoppedMilliseconds = 0; // Holds the number of milliseconds paused
+                // Get time from the chronometer
+                String chronoText = timeWaitingChronometer.getText().toString();
+                String array[] = chronoText.split(":");
+                if (array.length == 2) {    // Find the seconds
+                    stoppedMilliseconds = Integer.parseInt(array[0]) * 60 * 1000
+                            + Integer.parseInt(array[1]) * 1000;
+                } else if (array.length == 3) { // Find the minutes
+                    stoppedMilliseconds = Integer.parseInt(array[0]) * 60 * 60 * 1000
+                            + Integer.parseInt(array[1]) * 60 * 1000
+                            + Integer.parseInt(array[2]) * 1000;
+                }
+
+                // Amount of time elapsed since the start button was pressed, minus the time paused
+                timeWaitingChronometer.setBase(SystemClock.elapsedRealtime() - stoppedMilliseconds);
+
+                // Set the number of seconds you have waited This would be set for minutes in the
+                // real world obviously. That can be found in array[2]
+                secondsYouWaited = Long.parseLong(array[1]);
+                updateTipBasedOnTimeWaited(secondsYouWaited);
+                timeWaitingChronometer.start(); // Start the chronometer
+            }
+        });
+        pauseChronometerButton.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                timeWaitingChronometer.stop();
+            }
+        });
+
+        resetChronometerButton.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                timeWaitingChronometer.setBase(SystemClock.elapsedRealtime());
+                secondsYouWaited = 0;   // Reset milliseconds waited back to 0
+            }
+        });
+    }
+
+    private void updateTipBasedOnTimeWaited(long secondsYouWaited){
+
+        // If spent < 10 sec, add 2 to the tip. If spent > 10 sec, subtract 2
+        checklistValues[9] = (secondsYouWaited > 10)?-2:2;
+        // Calculate tip using the waitress checklist options
+
+        setTipFromWaitressChecklist();  // Calculate tip using the waitress checklist options
+        updateTipAndFinalBill();        // Update all the other EditTexts
+    }
+
+    // ---- END OF NEW STUFF ----------
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
